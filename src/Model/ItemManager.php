8bb49857b3338888faa0abac4e87e1646510564e
Romain Clair <romain.clair@wildcodeschool.fr>
2020-11-06 11:11:20 +0100

    updating to composer 2 and fix consequences

--- src/Model/ItemManager.php
+++ src/Model/ItemManager.php
@@ -19,6 +19,7 @@ class ItemManager extends AbstractManager
      *
      */
     public const TABLE = 'item';
+    public const DATABASE_ERROR = -1;
 
     /**
      *  Initializes this class.
@@ -42,6 +43,7 @@ class ItemManager extends AbstractManager
         if ($statement->execute()) {
             return (int)$this->pdo->lastInsertId();
         }
+        return self::DATABASE_ERROR;
     }
 
 
